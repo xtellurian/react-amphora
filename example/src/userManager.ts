51e1fd06d01ede5fdcbd67197dc753d8b5b98298
Rian Finnegan <rian@amphoradata.com>
2020-06-22 17:26:27 +1000

    feat: allow redirect to deployed version

--- example/src/userManager.ts
+++ example/src/userManager.ts
@@ -1,8 +1,25 @@
 import { createUserManager } from 'react-amphora'
 
+export const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+        // [::1] is the IPv6 localhost address.
+        window.location.hostname === '[::1]' ||
+        // 127.0.0.1/8 is considered localhost for IPv4.
+        window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+        )
+)
+
+let redirectUri =
+    'https://react-amphora-example.xtellurian.vercel.app/#/callback'
+
+if (isLocalhost) {
+    redirectUri = 'http://localhost:3000/#/callback'
+}
+
 const userManager = createUserManager({
-    clientId: '9dffa207-6e61-4f9c-8c14-b37c4fb81bb0',
-    redirectUri: 'http://localhost:3000/#/callback',
+    clientId: 'b022d07e-5867-48b5-99ff-d48d92c774a2',
+    redirectUri
 })
 
 export { userManager }
