c9a8877431e310e5a6d6df5f2bfedffd99f9eb1e
Rian Finnegan <rian@amphoradata.com>
2020-06-24 10:49:59 +1000

    refactor: typify use async reducer

---
+++ src/context/apis/useAsyncReducer.ts
@@ -0,0 +1,35 @@
+import * as React from 'react'
+// eslint-disable-next-line no-unused-vars
+import { ApiState } from './apiState'
+
+type asyncReducer<S extends { error?: any }, A> = (
+    state: S,
+    action: A
+) => Promise<S>
+
+const useAsyncReducer = <S extends ApiState, A>(
+    reducer: asyncReducer<S, A>,
+    initialState: S
+): [S, (action: any) => Promise<void>] => {
+    const [state, setState] = React.useState(initialState)
+    const dispatch = async (action: any) => {
+        setState({ ...state, isLoading: true })
+        const result = reducer(state, action)
+        if (typeof result.then === 'function') {
+            try {
+                const newState = await result
+                setState(newState)
+            } catch (err) {
+                setState({ ...state, error: err })
+            }
+        } else {
+            // this is when the reducer isnt actually a promise
+            throw Error('Error in Async reducer')
+            // setState(result)
+        }
+    }
+
+    return [state, dispatch]
+}
+
+export default useAsyncReducer
