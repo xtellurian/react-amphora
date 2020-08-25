7bd3ed9213b54e765c76f9c370fb18c5062f39bd
Rian Finnegan <rian@amphoradata.com>
2020-08-25 15:24:48 +1000

    feat: expose the underlying user manager config

--- src/auth/userManager.tsx
+++ src/auth/userManager.tsx
@@ -9,30 +9,23 @@ type Scope =
     | 'amphora'
     | 'offline_access'
 
-export interface OAuthConfig {
+export interface OAuthConfig extends UserManagerSettings {
     authority?: string
     clientId: string
     redirectUri: string
     scopes?: Scope[]
     silentRedirectUri?: string
-    automaticSilentRenew?: boolean
-    filterProtocolClaims?: boolean
-    loadUserInfo?: boolean
-    monitorSession?: boolean
 }
 
 function getSettings(config: OAuthConfig): UserManagerSettings {
     return {
+        ...config,
         authority: config.authority || 'https://identity.amphoradata.com',
         client_id: config.clientId,
         redirect_uri: config.redirectUri,
         silent_redirect_uri: config.silentRedirectUri,
         scope: config.scopes ? config.scopes.join(' ') : 'openid profile',
-        response_type: 'code',
-        automaticSilentRenew: config.automaticSilentRenew,
-        filterProtocolClaims: config.filterProtocolClaims,
-        loadUserInfo: config.loadUserInfo,
-        monitorSession: config.monitorSession
+        response_type: 'code'
     }
 }
 
